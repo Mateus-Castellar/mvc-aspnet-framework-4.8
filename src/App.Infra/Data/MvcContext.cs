c3cf42fdb37d751dd9694607a3c726bbbff4accb
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-26 15:55:09 -0300

    refactor: Melhorias dos mapeamentos e gerando base de dados

--- src/App.Infra/Data/MvcContext.cs
+++ src/App.Infra/Data/MvcContext.cs
@@ -2,13 +2,17 @@
 using App.Business.Models.Produtos;
 using App.Infra.Data.Mappings;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace App.Infra.Data
 {
     public class MvcContext : DbContext
     {
         public MvcContext() : base("DefaultConnection")
-        { }
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
 
         public DbSet<Produto> Produtos { get; set; }
         public DbSet<Fornecedor> Fornecedores { get; set; }
@@ -16,9 +20,20 @@ namespace App.Infra.Data
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<string>()
+                .Configure(p => p
+                    .HasColumnType("varchar")
+                    .HasMaxLength(100));
+
             modelBuilder.Configurations.Add(new FornecedorMapping());
             modelBuilder.Configurations.Add(new EnderecoMapping());
             modelBuilder.Configurations.Add(new ProdutoMapping());
+
+            base.OnModelCreating(modelBuilder);
         }
     }
 }
