5edfffd0f5dc8968be6bd1f81a220b1867ab20f2
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-27 03:04:18 -0300

    feat: Servico base para persistencia e manipulacao de fornecedores

---
+++ src/App.Business/Core/Services/BaseService.cs
@@ -0,0 +1,16 @@
+﻿using App.Business.Core.Models;
+using FluentValidation;
+
+namespace App.Business.Core.Services
+{
+    public abstract class BaseService
+    {
+        protected bool ExecutarValidacao<TValidation, TEntity>(TValidation validacao, TEntity entidade)
+            where TValidation : AbstractValidator<TEntity>
+            where TEntity : Entity
+        {
+            var validator = validacao.Validate(entidade);
+            return validator.IsValid;
+        }
+    }
+}
