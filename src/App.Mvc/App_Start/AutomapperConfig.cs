24b6d3fda3631520e2d308eb23d4d085d6759ab6
Mateus Castellar <mateuscastellar@outlook.com>
2022-10-27 06:20:07 -0300

    feat: Controller/Views basicas de produtos e configuracao do automapper

---
+++ src/App.Mvc/App_Start/AutomapperConfig.cs
@@ -0,0 +1,36 @@
+﻿using App.Business.Models.Fornecedores;
+using App.Business.Models.Produtos;
+using App.Mvc.ViewModels;
+using AutoMapper;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace App.Mvc.App_Start
+{
+    public class AutomapperConfig
+    {
+        public static MapperConfiguration GetMapperConfiguration()
+        {
+            //executa no momento que a aplicação sobe (reflection)
+            var profiles = Assembly.GetExecutingAssembly().GetTypes()
+                .Where(lbda => typeof(Profile).IsAssignableFrom(lbda));
+
+            return new MapperConfiguration(config =>
+            {
+                foreach (var profile in profiles)
+                    config.AddProfile(Activator.CreateInstance(profile) as Profile);
+            });
+        }
+    }
+
+    public class AutomapperProfile : Profile
+    {
+        public AutomapperProfile()
+        {
+            CreateMap<Fornecedor, FornecedorViewModel>().ReverseMap();
+            CreateMap<Endereco, EnderecoViewModel>().ReverseMap();
+            CreateMap<Produto, ProdutoViewModel>().ReverseMap();
+        }
+    }
+}
---
+++ src/App.Mvc/Controllers/ProdutosController.cs
@@ -0,0 +1,132 @@
+﻿using App.Business.Models.Produtos;
+using App.Business.Models.Produtos.Services;
+using App.Mvc.ViewModels;
+using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace App.Mvc.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        private readonly IProdutoRepository _produtoRepository;
+        private readonly IProdutoService _produtoService;
+        private readonly IMapper _mapper;
+
+        public ProdutosController(IProdutoRepository produtoRepository, IProdutoService produtoService,
+            IMapper mapper)
+        {
+            _produtoRepository = produtoRepository;
+            _produtoService = produtoService;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        [Route("lista-de-produtos")]
+        public async Task<ActionResult> Index()
+        {
+            return View(_mapper.Map<IEnumerable<ProdutoViewModel>>(await
+                _produtoRepository.ObterTodos()));
+        }
+
+        [HttpGet]
+        [Route("dados-do-produto/{id:guid}")]
+        public async Task<ActionResult> Details(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+
+            if (produtoViewModel is null) return HttpNotFound();
+
+            return View(produtoViewModel);
+        }
+
+        [HttpGet]
+        [Route("novo-produto")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("novo-produto")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create(ProdutoViewModel produtoViewModel)
+        {
+            if (ModelState.IsValid is false) return View(produtoViewModel);
+
+            await _produtoService.Adicionar(_mapper.Map<Produto>(produtoViewModel));
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        [Route("editar-produto/{id:guid}")]
+        public async Task<ActionResult> Edit(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+
+            if (produtoViewModel is null) return HttpNotFound();
+
+            return View(produtoViewModel);
+        }
+
+        [HttpPost]
+        [Route("editar-produto/{id:guid}")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit(ProdutoViewModel produtoViewModel)
+        {
+            if (ModelState.IsValid is false) return View(produtoViewModel);
+
+            await _produtoService.Atualizar(_mapper.Map<Produto>(produtoViewModel));
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        [Route("excluir-produto/{id:guid}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+
+            if (produtoViewModel is null) return HttpNotFound();
+
+            return View(produtoViewModel);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [Route("excluir-produto/{id:guid}")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(Guid id)
+        {
+            var produtoViewModel = await ObterProduto(id);
+
+            if (produtoViewModel is null) return HttpNotFound();
+
+            await _produtoService.Remover(id);
+
+            return RedirectToAction("Index");
+        }
+
+        private async Task<ProdutoViewModel> ObterProduto(Guid id)
+        {
+            var produto = _mapper.Map<ProdutoViewModel>(await
+                _produtoRepository.ObterProdutoFornecedor(id));
+
+            return produto;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                _produtoRepository.Dispose();
+                _produtoService.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
